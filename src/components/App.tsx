eb229e7a68e4e581dbdf2cdc3d1863a0558adefd
Akihiro <marsstay0729@gmail.com>
2021-04-13 11:58:08 +0900

    add reset css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from '../assets/styles/App.module.scss';
+import { Toolbox } from './organisms/Toolbox';
 
 function App() {
   return (
     <div className="App">
-      <div className={classes.hoge}>hogehogemouse</div>
+      <h1 className={classes.title}>Tool Box</h1>
+      <Toolbox />
     </div>
   );
 }
---
+++ src/components/organisms/Toolbox.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from '../assets/styles/App.module.scss';
+
+export const Toolbox = () => {
+  return <div className="Toolbox">tool</div>;
+};
