039990998689807ec55350cb722458ab30942329
Akihiro <marsstay0729@gmail.com>
2021-04-13 11:17:11 +0900

    Setting style css

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from '../assets/styles/App.module.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className={classes.hoge}>hogehogemouse</div>
+    </div>
+  );
+}
+
+export default App;
