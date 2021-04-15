519d7725ad3114489c87714017aa7a117ccfaf17
Akihiro <marsstay0729@gmail.com>
2021-04-15 17:33:16 +0900

    Add animation on top page

--- src/components/organisms/Toolbox.tsx
+++ src/components/organisms/Toolbox.tsx
@@ -3,11 +3,13 @@ import classes from '../../assets/styles/Toolbox.module.scss';
 
 export const Toolbox = () => {
   return (
-    <div className="Toolbox">
+    <div className={classes.toolbox}>
       {/* molecules:アイテム */}
       <div className={classes.contentWrapper}>
-        <h2>文字数カウント</h2>
-        <p>これは文字数カウント</p>
+        <div>
+          <h2>文字数カウント</h2>
+          <p>これは文字数カウント</p>
+        </div>
       </div>
     </div>
   );
